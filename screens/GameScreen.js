d808c807e81ccc257a3bdd7ba2702bc1f4087e9e
Mason Chang <changmason@gmail.com>
2021-11-03 20:34:20 +0800

    Add ScreenOrientation offered by Expo

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { View, Alert, StyleSheet, FlatList, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 import Card from "../components/Card";
 import BodyText from "../components/BodyText";
@@ -29,6 +30,8 @@ const renderListItem = (listLength, itemData) => {
 };
 
 const GameScreen = (props) => {
+  // ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
